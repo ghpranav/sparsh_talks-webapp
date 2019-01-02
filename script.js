734c7bc5111db81f8fd4da2814aaaf847a342948
ghpranav <bedrepranav@gmail.com>
2019-01-03 01:24:12 +0530

    sparsh: Add more design & clean-up old code

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
+
