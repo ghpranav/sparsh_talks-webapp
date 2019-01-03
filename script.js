c71b894410fe3f494fb52ea4895bf05430ea393b
ghpranav <bedrepranav@gmail.com>
2019-01-03 08:45:01 +0530

    navbar: Collapse nav on click

--- script.js
+++ script.js
@@ -34,4 +34,8 @@ $(document).ready(function(){
   });
 });
 
-
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+        $(this).collapse('hide');
+    }
+});
