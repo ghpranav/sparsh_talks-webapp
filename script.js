e1cde0b5da64289233ad10e672a9fd55d561015c
Pranav Bedre <bedrepranav@gmail.com>
2019-02-01 19:23:42 +0530

    gallery: Fix broken image slider

--- script.js
+++ script.js
@@ -31,17 +31,9 @@ $(document).ready(function () {
     speed: 300,
     autoplay: true,
     autoplaySpeed: 2000,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          dots: true
-        }
-      },
-      {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
